019fa4e95f47109eb5dbb9fbf061ccac19845eb6
klmartinez803-byte <klmartinez803@gmail.com>
2025-08-29 18:15:50 -0400

    Delete wedding_website directory

--- wedding_website/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("rsvp-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("response-message").textContent = "Thank you for your RSVP!";
-    this.reset();
-});
